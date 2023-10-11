b421d2630faeb0af66eaa8b1be89fc2de450b319
RodrigoMachadodaSilva <87843482+RodrigoMachadodaSilva@users.noreply.github.com>
2023-10-11 16:29:45 -0300

    Adicionando o código JavaScript para geração de eventos

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,46 @@
-console.log("Opla");
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+const qrCodeInput = document.querySelector("#qr-form input");
+const qrCodeImg = document.querySelector("#qr-code img");
+
+// Eventos
+
+// Gerar QR Code
+function gererateQrCode() {
+
+    const qrCodeInputValue = qrCodeInput.value;
+
+    if(!qrCodeInputValue) return;
+
+    qrCodeBtn.innerText = "Gerando código...";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+    qrCodeImg.addEventListener("load", () =>{
+        container.classList.add("active");
+        qrCodeBtn.innerText = "Código criado!...";
+    })
+
+}
+
+qrCodeBtn.addEventListener("click", () => {
+    gererateQrCode();
+
+})
+
+qrCodeInput.addEventListener("keydown", (e) => {
+    if(e.code==="Enter") {
+        gererateQrCode();
+    }
+
+
+})
+
+// Limpar área do QR Code
+
+qrCodeInput.addEventListener("Keyup", () => {
+    if(!qrCodeInput.value) {
+        container.classList.remove("active");
+        qrCodeBtn.innerText = "Gerar QR Code...";
+    }
+})
